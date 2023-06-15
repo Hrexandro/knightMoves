d4af1b5965b0f93e822a371e18b010cccbb28cfa
Domowe <marcin_nowak_hrx@o2.pl>
2023-06-15 20:36:28 +0200

    implement breadth-wise search, works only with increasing or decreasing fields one way

--- knightMoves.js
+++ knightMoves.js
@@ -19,12 +19,6 @@
 //     [4,3]
 
 
-
-//TO DO: TEST THE PROCESS SO THAT FILTERING LONGER PATHS WORKS CORRECTLY
-//IF STILL TAKES TOO LONG - TRY TO IMPLEMENT A DIFFERENT SEARCH ALGORITHM - COMPARE TO BINARY SEARCH TREES
-
-
-
 function makeSureNotToLeaveBoard(coordinateArray) {
   if (coordinateArray[0] > 7 || coordinateArray[0] < 0) {
     return false
@@ -64,8 +58,9 @@ function createBoard() {
   return result;
 }
 let board = createBoard();
-console.log(board)
-function knightMoves(start, destination, path = []) {
+//console.log(board)
+
+function knightMovesDepthwise(start, destination, path = []) {//old way of doin it
 
   let lengthOfPathToDestination = Infinity
   let currentShortestPathToDestination = null
@@ -122,14 +117,98 @@ function knightMoves(start, destination, path = []) {
   return `Function concluded successfully.`
 }
 
-//console.log(knightMoves([0, 0], [0, 0]))
-//console.log(knightMoves([1, 2], [0, 0]))
-//console.log(knightMoves([0, 0], [1, 2]))
-
-console.log(knightMoves([0, 0], [3, 3])) //[[0,0],[1,2],[3,3]] //mine returns [[0,0],[2,1],[3,3]]
-
-//console.log(knightMoves([3, 3], [0, 0]))// [[3,3],[1,2],[0,0]]
-
- //console.log(knightMoves([2, 2], [3, 4]))//one move
 
 
+
+function knightMoves(start, destination, path = []) {
+  let lengthOfPathToDestination = Infinity
+  let currentShortestPathToDestination = []
+  let shortestPathsToFields = []
+
+  let queue = [{ 
+    start, 
+    destination, 
+    path: []
+  }]
+
+  // console.log(queue[0].start)
+  for (let j = 0; j < board.length; j++) {
+    let fieldToAdd = board[j]
+    fieldToAdd.shortestDistanceFromStart = Infinity
+    shortestPathsToFields.push(fieldToAdd)
+  }
+
+  function knightMovesRecursion(start, destination, path = []) {
+    if (queue.length < 1){
+      return
+    }
+    let currentFieldOnFieldList = shortestPathsToFields.find((e)=>JSON.stringify(e.coordinates) === JSON.stringify(start))
+    // console.log("checking this error")
+    // console.log(destination)
+    // console.log(shortestPathsToFields)
+    // console.log(currentFieldOnFieldList)
+    if (currentFieldOnFieldList.shortestDistanceFromStart < path.length) {//if path is longer than shortest visited, return
+      return
+
+    } else if (currentFieldOnFieldList.shortestDistanceFromStart > path.length){//new path is shorter, update
+      shortestPathsToFields.find((e)=>JSON.stringify(e.coordinates) === JSON.stringify(start)).shortestDistanceFromStart = path.length //don't copy because we are changing the original
+    } 
+
+    if (start.toString() === destination.toString()) {//searched field found
+      if (path.length < lengthOfPathToDestination) {
+        currentShortestPathToDestination = path
+        lengthOfPathToDestination = currentShortestPathToDestination.length
+      }
+      return
+    } else {//continue
+
+      let availableMoves = board.find((e) => e.coordinates.toString() === start.toString()).possibleMoves
+
+      for (let i = 0; i < availableMoves.length; i++) {
+        if (!path.some((e) => JSON.stringify(e) === JSON.stringify(availableMoves[i]))) {
+          queue.push({
+            start: availableMoves[i],
+            destination: destination,
+            path: [...path, start]
+          })
+          //knightMovesRecursion(availableMoves[i], destination, [...path, start])
+        }
+
+      }
+
+
+    }
+    queue.shift()
+    knightMovesRecursion(queue[0].start, queue[0].destination, queue[0].path)
+    console.log(queue)
+  }//end of recursion
+  console.log(queue)
+  knightMovesRecursion(queue[0].start, queue[0].destination, queue[0].path)
+  console.log("ending the whole function")
+
+
+  console.log("check")
+  console.log(currentShortestPathToDestination.length)
+  console.log(currentShortestPathToDestination.length - 1)
+  console.log(`=> You made it in ${currentShortestPathToDestination.length + (currentShortestPathToDestination.length === 1 ? ' move' : ' moves')}!  Here's your path:`)
+  currentShortestPathToDestination.forEach((e) => console.log(e))
+  console.log(destination)
+
+  return `Function concluded successfully.`
+
+}
+  
+  
+  
+  //console.log(knightMoves([0, 0], [0, 0]))
+  //console.log(knightMoves([1, 2], [0, 0]))
+  //console.log(knightMoves([0, 0], [1, 2]))
+  
+  //console.log(knightMoves([0, 0], [3, 3])) //[[0,0],[1,2],[3,3]] //mine returns [[0,0],[2,1],[3,3]]
+  
+  console.log(knightMoves([3, 3], [0, 0]))// [[3,3],[1,2],[0,0]]
+  
+  //console.log(knightMoves([2, 2], [3, 4]))//one move
+
+
+  console.log(knightMoves([0, 0], [5, 2]))
